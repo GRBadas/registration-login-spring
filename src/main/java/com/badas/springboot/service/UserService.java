64f6c8d38096153e2076629944446c5eab70d2f0
Gabriel Badaró <grbadas@gmail.com>
2023-02-14 17:20:19 -0300

    update

--- src/main/java/com/badas/springboot/service/UserService.java
+++ src/main/java/com/badas/springboot/service/UserService.java
@@ -1,9 +1,11 @@
 package com.badas.springboot.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.badas.springboot.model.User;
 import com.badas.springboot.web.dto.UserRegistrationDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 	User save(UserRegistrationDto registrationDto);
 
 }
