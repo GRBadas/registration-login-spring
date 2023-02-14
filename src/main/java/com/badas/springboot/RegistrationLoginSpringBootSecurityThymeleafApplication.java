29fc0388097678f76b8c9716c7d57d951bfd62ff
Gabriel Badaró <grbadas@gmail.com>
2023-02-14 16:43:04 -0300

    update

--- src/main/java/com/badas/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
+++ src/main/java/com/badas/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -2,12 +2,15 @@ package com.badas.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class RegistrationLoginSpringBootSecurityThymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class, args);
 	}
+	
+	
 
 }
